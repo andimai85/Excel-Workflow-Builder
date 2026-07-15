0391e70f4587e1ff3ea903850be73da3a8dd1da7
andimai85 <andreasmai23@gmail.com>
2026-07-15 14:30:39 +0200

    Add files via upload

---
+++ electron/main.cjs
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, shell } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    title: "Workflow-Generator",
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.setMenuBarVisibility(false);
+  win.loadFile(path.join(__dirname, "..", "electron-spa", "dist", "index.html"));
+
+  // Open external links in the system browser.
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: "deny" };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
